de3ceb73b3f12faff38c4cc12cc5e057e8d060bc
miavendemiati <mia.vendemiati@itiszuccante.edu.it>
2026-02-04 10:47:28 +0100

    create carte che non si vedono (da sistemare)

--- gioco.py
+++ gioco.py
@@ -7,6 +7,7 @@ altezza = 768
 titolo = "solitario"
 
 
+
 class Game(arcade.Window):
     def __init__(self, larghezza, altezza, titolo):
         super().__init__(larghezza, altezza, titolo)
@@ -36,6 +37,61 @@ class Game(arcade.Window):
     
     def on_mouse_release(self, x: float, y: float, dx: float, dy: float):
         pass
+    
+class Carta(arcade.Sprite):
+    def __init__(self, seme, valore, filename, scala = 0.5):
+        super().__init__(filename, scala)
+        self.seme = seme
+        self.valore = valore
+        
+        self.front_texture = arcade.load_texture
+        self.back_texture = arcade.load_texture("giochino/assets/card_back.png")
+        
+        self.scoperta = False
+        self.texture = self.back_texture
+    
+    def colore(self):
+        if self.seme in ["cuori", "quadri"]:
+            return "rosso"
+        return "nero"
+    
+def crea_mazzo():
+    semi = ["cuori", "quadri", "fiori", "picche"]
+    nomi = {
+        1: "asso",
+        11: "jack",
+        12: "regina",
+        13: "re"
+    }
+        
+    mazzo = []
+    
+    for seme in semi:
+        for valore in range(1, 14):
+                nome = nomi.get(valore, str(valore))
+                filename = f"assets/carte/{nome}_{seme}.png"
+                carta = Carta(seme, valore, filename)
+                mazzo.append(carta)
+    
+    random.shuffle(mazzo)
+    return mazzo
+    
+def setup(self):
+    self.sfondo()
+    self.lista_carte = arcade.SpriteList()
+    
+    mazzo = crea_mazzo()   
+    print(len(mazzo)) 
+    
+    x = 100
+    y = 600
+    
+    for carta in mazzo:
+        carta.center_x = x
+        carta.center_y = y
+        self.lista_carte.apend(carta)
+        x += 0.5
+    
     
 def main():
         Game(larghezza, altezza, titolo)
