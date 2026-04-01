6aa00a1a7c9ab1ef91662396b2be63f72b0a610a
miavendemiati <mia.vendemiati@itiszuccante.edu.it>
2026-04-01 11:05:15 +0200

    sistemati dei problemini

--- gioco.py
+++ gioco.py
@@ -34,7 +34,7 @@ def crea_mazzo():
             filename = f"assets/carte/solitaire/individuals/{valore}_{seme}.png"
             carta = Carta(seme, valore, filename)
             mazzo.append(carta)
-    print(filename)
+    print(carta.seme, carta.valore)
 
     random.shuffle(mazzo)
     return mazzo
@@ -53,74 +53,76 @@ class Game(arcade.Window):
         self.start_x_colonne = 100
         self.start_y_colonne = 650
         self.spazio_y_carte = 30
+        self.pile_selezionata = None
+        self.colonna_originale = None
+        self.scarti = []
         
     def trova_posizione(self, carta):
         for i, colonna in enumerate(self.colonne):
             if carta in colonna:
                 indice_carta = colonna.index(carta)
                 return i, indice_carta
-            if carta in self.scarti:
-                return 7, self.scarti.index(carta)
+            
+        if carta in self.scarti:
+            return 7, self.scarti.index(carta)
             
         return None, None
         
     def on_mouse_press(self, x, y, button, modifiers):
-        if button == arcade.MOUSE_BUTTON_LEFT:
-            for carta in reversed(self.lista_carte):
-                if carta.collides_with_point((x, y)) and carta.scoperta:
-                    self.carta_selezionata = carta
-                    
-                    col, idx = self.trova_posizione(carta)
-                    print(col, idx)
-                    self.pile_selezionata = self.colonne[col][idx:]
-                    self.colonna_originale = col
-                    if col == 7:
-                        self.pile_selezionata = self.scarti[idx:]
-                    
-                    self.offset_x = self.pile_selezionata[0].center_x - x
-                    self.offset_y = self.pile_selezionata[0].center_y - y
-                    
-                    for carta in self.pile_selezionata:          
-                        carta.remove_from_sprite_lists()
-                        self.lista_carte.append(carta)
-                    
-            for carta in reversed(self.lista_carte):
-                if carta.collides_with_point((x, y)) and carta.scoperta:
-                    self.carta_selezionata = carta
-                    self.pile_selezionata = [carta]
-                    self.offset_x = carta.center_x - x
-                    self.offset_y = carta.center_y - y
-                    
-                    break
-
-            print(len(self.mazzo_pesca))
-            if 60 < x < 140 and 60 < y < 140:
-                if self.mazzo_pesca:
-                    carta = self.mazzo_pesca.pop()
-
-                    carta.scoperta = True
-                    carta.texture = carta.front_texture
-
-                    self.scarti.append(carta)
-                    
-                    carta.remove_from_sprite_lists()
-                    self.lista_carte.append(carta)
-
-                    carta.center_x = 200
+        if button != arcade.MOUSE_BUTTON_LEFT:
+            return
+        
+        if 60 < x < 140 and 60 < y < 140:
+            if self.mazzo_pesca:
+                carta = self.mazzo_pesca.pop()
+                carta.scoperta = True
+                carta.texture = carta.front_texture
+            
+                self.scarti.append(carta)
+            
+                carta.center_x = 200
+                carta.center_y = 100
+            
+                carta.remove_from_sprite_lists()
+                self.lista_carte.append(carta)
+            else:
+                self.mazzo_pesca = self.scarti[::-1]
+                self.scarti = []
+            
+                for carta in self.mazzo_pesca:
+                    carta.scoperta = False
+                    carta.texture = carta.back_texture
+                    carta.center_x = 100
                     carta.center_y = 100
-
-                    if carta not in self.lista_carte:
-                        self.lista_carte.append(carta)
+                
+            return
+    
+        for carta in reversed(self.lista_carte):
+            if carta.collides_with_point((x, y)) and carta.scoperta:
+            
+                col, idx = self.trova_posizione(carta)
+            
+                if col is None:
+                    continue
+            
+                self.colonna_originale = col
+            
+                if col == 7:
+                    if idx != len(self.scarti) - 1:
+                        return
+                    self.pile_selezionata = [self.scarti[idx]]
                 else:
-                    self.mazzo_pesca = self.scarti[::-1]
-                    self.scarti = []
-                    
-                    for carta in self.mazzo_pesca:
-                        carta.scoperta =  False
-                        carta.texture = carta.back_texture
-                        carta.center_x = 100
-                        carta.center_y = 100
-                            
+                    self.pile_selezionata = self.colonne[col][idx:]
+                
+                self.offset_x = self.pile_selezionata[0].center_x - x
+                self.offset_y = self.pile_selezionata[0].center_y - y
+            
+                for c in self.pile_selezionata:
+                    c.remove_from_sprite_lists()
+                    self.lista_carte.append(c)
+                
+                return
+           
     def on_mouse_motion(self, x, y, dx, dy):
         if self.pile_selezionata:
             for i, carta in enumerate(self.pile_selezionata):
@@ -128,35 +130,35 @@ class Game(arcade.Window):
                 carta.center_y = (y + self.offset_y) - i * self.spazio_y_carte
     
     def on_mouse_release(self, x, y, button, modifiers):
-        if button == arcade.MOUSE_BUTTON_LEFT:
-            self.carta_selezionata = None
+        if button != arcade.MOUSE_BUTTON_LEFT:
+            return
 
         if not self.pile_selezionata:
             return
         
-        if col_dest > 6:  # se proviene dagli scarti
-            self.scarti = self.scarti[:-len(self.pile_selezionata)]
-        else:
-            self.colonne[self.colonna_originale] = self.colonne[self.colonna_originale][:-len(self.pile_selezionata)]
-        
         col_dest = int((x - self.start_x_colonne + self.spazio_x_colonne // 2) / self.spazio_x_colonne)         
         col_dest = max(0, min(6, col_dest))  # limita tra 0 e 6 colonne
-        self.colonne[self.colonna_originale] = self.colonne[self.colonna_originale][:-len(self.pile_selezionata)]
-        self.colonne[col_dest].extend(self.pile_selezionata)
         
-        for i, carta in enumerate(self.colonne[col_dest][-len(self.pile_selezionata):]):
+        if self.colonna_originale == 7:
+            self.scarti = self.scarti[:-len(self.pile_selezionata)]
+        else:
+            self.colonne[self.colonna_originale] = self.colonne[self.colonna_originale][:-len(self.pile_selezionata)] 
+        
+        self.colonne[col_dest].extend(self.pile_selezionata)
+               
+        for i, carta in enumerate(self.colonne[col_dest]):
             carta.center_x = self.start_x_colonne + col_dest * self.spazio_x_colonne
-            carta.center_y = self.start_y_colonne - (len(self.colonne[col_dest]) - len(self.pile_selezionata) + i) * self.spazio_y_carte
-
-        colonna = self.colonne[self.colonna_originale]
-
-        if len(colonna) >0:
-            ultima = colonna[-1]
-
-            if not ultima.scoperta:
-                ultima.scoperta = True
-                ultima.texture = ultima.front_texture
-
+            carta.center_y = self.start_y_colonne - i * self.spazio_y_carte
+        
+        if self.colonna_originale != 7:
+            colonna = self.colonne[self.colonna_originale]
+            if len(colonna) >0:
+                ultima = colonna[-1]
+                if not ultima.scoperta:
+                    ultima.scoperta = True
+                    ultima.texture = ultima.front_texture
+                    
+        self.carta_selezionata = None
         self.colonna_originale = None
         self.pile_selezionata = None
 
